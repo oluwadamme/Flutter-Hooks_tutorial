674624a7d545e6404086e4ef56232f04a2cc0d7e
oluwadamme <adeniyidamilola246@gmail.com>
2022-04-13 18:27:09 +0100

    useFuture and useMemoized

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 
 void main() {
@@ -22,36 +23,46 @@ class MyApp extends StatelessWidget {
   }
 }
 
-Stream<String> getTime() => Stream.periodic(
-    const Duration(seconds: 1), (_) => DateTime.now().toIso8601String());
+// Stream<String> getTime() => Stream.periodic(
+//     const Duration(seconds: 1), (_) => DateTime.now().toIso8601String());
+
+const url =
+    'https://images.pexels.com/photos/2116388/pexels-photo-2116388.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500';
 
 class HomePage extends HookWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    final dateTime = useStream(getTime());
-    final controller = useTextEditingController();
-    final text = useState('');
-    useEffect(() {
-      controller.addListener(() {
-        text.value = controller.text;
-      });
-      return null;
-    }, [controller]);
+    // final dateTime = useStream(getTime());
+    // final controller = useTextEditingController();
+    // final text = useState('');
+    // useEffect(() {
+    //   controller.addListener(() {
+    //     text.value = controller.text;
+    //   });
+    //   return null;
+    // }, [controller]);
+
+    final future = useMemoized(() => NetworkAssetBundle(Uri.parse(url))
+        .load(url)
+        .then((data) => data.buffer.asUint8List())
+        .then((data) => Image.memory(data)));
+
+    final snapshot = useFuture(future);
     return Scaffold(
       appBar: AppBar(
-        title: Text(dateTime.data ?? 'This is home'),
+        title: const Text('This is home'),
       ),
       body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          TextFormField(
-            controller: controller,
-          ),
-          Text('Typing: ${text.value}')
-        ],
+        children: [snapshot.data].compactMap().toList(),
       ),
     );
   }
+}
+
+// this extension is to prevent null value
+extension CompactMap<T> on Iterable<T?> {
+  Iterable<T> compactMap<E>([E? Function(T?)? transform]) =>
+      map(transform ?? (e) => e).where((e) => e != null).cast();
 }
