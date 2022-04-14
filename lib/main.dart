d3e83a628784d7d4c4c312f1bd4d8bf16a71c32e
oluwadamme <adeniyidamilola246@gmail.com>
2022-04-14 14:07:26 +0100

    useListenable and useMemoized

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 
 void main() {
@@ -26,8 +27,8 @@ class MyApp extends StatelessWidget {
 // Stream<String> getTime() => Stream.periodic(
 //     const Duration(seconds: 1), (_) => DateTime.now().toIso8601String());
 
-const url =
-    'https://images.pexels.com/photos/2116388/pexels-photo-2116388.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500';
+// const url =
+//     'https://images.pexels.com/photos/2116388/pexels-photo-2116388.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500';
 
 class HomePage extends HookWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -44,19 +45,20 @@ class HomePage extends HookWidget {
     //   return null;
     // }, [controller]);
 
-    final future = useMemoized(() => NetworkAssetBundle(Uri.parse(url))
-        .load(url)
-        .then((data) => data.buffer.asUint8List())
-        .then((data) => Image.memory(data)));
+    // final future = useMemoized(() => NetworkAssetBundle(Uri.parse(url))
+    //     .load(url)
+    //     .then((data) => data.buffer.asUint8List())
+    //     .then((data) => Image.memory(data)));
 
-    final snapshot = useFuture(future);
+    // final snapshot = useFuture(future);
+
+    final countDown = useMemoized(() => CountDown(from: 20));
+    final notifier = useListenable(countDown);
     return Scaffold(
       appBar: AppBar(
         title: const Text('This is home'),
       ),
-      body: Column(
-        children: [snapshot.data].compactMap().toList(),
-      ),
+      body: Text(notifier.value.toString()),
     );
   }
 }
@@ -65,4 +67,21 @@ class HomePage extends HookWidget {
 extension CompactMap<T> on Iterable<T?> {
   Iterable<T> compactMap<E>([E? Function(T?)? transform]) =>
       map(transform ?? (e) => e).where((e) => e != null).cast();
+}
+
+class CountDown extends ValueNotifier<int> {
+  late StreamSubscription sub;
+  CountDown({required int from}) : super(from) {
+    sub = Stream.periodic(const Duration(seconds: 1), (v) => from - v)
+        .takeWhile((value) => value >= 0)
+        .listen((value) {
+      this.value = value;
+    });
+  }
+
+  @override
+  void dispose() {
+    sub.cancel();
+    super.dispose();
+  }
 }
