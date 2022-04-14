e420287a55cc512f28a671cdc64750d3c6a827b0
oluwadamme <adeniyidamilola246@gmail.com>
2022-04-14 15:40:56 +0100

    useReducer

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'dart:math';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
@@ -31,43 +32,112 @@ const url =
     'https://images.pexels.com/photos/2116388/pexels-photo-2116388.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500';
 const imageHeight = 300.0;
 
+enum Action {
+  rotateLeft,
+  rotateRight,
+  moreVisible,
+  lessVisible,
+}
+
+@immutable
+class State {
+  final double rotationDeg;
+  final double alpha;
+
+  const State({required this.rotationDeg, required this.alpha});
+
+  const State.zero()
+      : rotationDeg = 0.0,
+        alpha = 1.0;
+
+  State rotateRight() => State(
+        alpha: alpha,
+        rotationDeg: rotationDeg + 10.0,
+      );
+  State rotateLeft() => State(
+        alpha: alpha,
+        rotationDeg: rotationDeg - 10.0,
+      );
+  State increaseAlpha() => State(
+        alpha: min(alpha + 0.1, 1.0),
+        rotationDeg: rotationDeg,
+      );
+  State decreaseAlpha() => State(
+        alpha: max(alpha - 0.1, 0.0),
+        rotationDeg: rotationDeg,
+      );
+}
+
+State reducer(State oldState, Action? action) {
+  switch (action) {
+    case Action.rotateLeft:
+      return oldState.rotateLeft();
+    case Action.rotateRight:
+      return oldState.rotateRight();
+    case Action.moreVisible:
+      return oldState.increaseAlpha();
+    case Action.lessVisible:
+      return oldState.decreaseAlpha();
+    case null:
+      return oldState;
+  }
+}
+
 class HomePage extends HookWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    late final StreamController<double> controller;
-
-    controller = useStreamController<double>(onListen: () {
-      controller.sink.add(0.0);
-    });
+    final store = useReducer<State, Action?>(reducer,
+        initialState: const State.zero(), initialAction: null);
     return Scaffold(
       appBar: AppBar(
         title: const Text('This is home'),
       ),
-      body: StreamBuilder<double>(
-          stream: controller.stream,
-          builder: (context, snapshot) {
-            if (!snapshot.hasData) {
-              return const LinearProgressIndicator();
-            } else {
-              final rotation = snapshot.data ?? 0.0;
-              return GestureDetector(
-                onTap: () {
-                  controller.sink.add(rotation + 10.0);
+      body: Column(
+        children: [
+          Row(
+            children: [
+              TextButton(
+                onPressed: () {
+                  store.dispatch(Action.rotateLeft);
                 },
-                child: RotationTransition(
-                  turns: AlwaysStoppedAnimation(rotation / 360),
-                  child: Center(
-                    child: Image.network(
-                      url,
-                      height: imageHeight,
-                    ),
-                  ),
-                ),
-              );
-            }
-          }),
+                child: const Text('Rotate Left'),
+              ),
+              TextButton(
+                onPressed: () {
+                  store.dispatch(Action.rotateRight);
+                },
+                child: const Text('Rotate Right'),
+              ),
+              TextButton(
+                onPressed: () {
+                  store.dispatch(Action.moreVisible);
+                },
+                child: const Text('More Visible'),
+              ),
+              TextButton(
+                onPressed: () {
+                  store.dispatch(Action.lessVisible);
+                },
+                child: const Text('Less Visible'),
+              )
+            ],
+          ),
+          const SizedBox(
+            height: 100,
+          ),
+          Opacity(
+            opacity: store.state.alpha,
+            child: RotationTransition(
+                turns: AlwaysStoppedAnimation(store.state.rotationDeg / 360.0),
+                child: Image.network(
+                  url,
+                  height: 400,
+                )),
+          ),
+        ],
+      ),
     );
   }
 }
